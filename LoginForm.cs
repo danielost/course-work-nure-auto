abfde932d7c83b66ff5767df65d86d3a1a687893
Daniel Ostapchenko <danya.ostap@gmail.com>
2021-05-12 23:01:15 +0300

    init commit

---
+++ LoginForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CourseWork
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile("C:\\Users\\Daniel\\Desktop\\2 Семестр\\ООП\\Курсовая\\CourseWork\\assets\\logo.bmp");
+        }
+    }
+}
